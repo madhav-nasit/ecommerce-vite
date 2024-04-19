d2c6b319bb3ea3a2755a1a8006ec7aec3a16278f
Madhav Nasit <madhavn@mindfiresolutions.com>
2024-04-19 11:10:59 +0530

    feat: Setup tailwind css with theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#141718',
+        background: '#FFFFFF',
+        secondary: '#F3F5F7',
+        card: '#FFFFFF',
+        color: '#141718',
+        light: '#6C7275',
+        error: '#FF5630',
+        success: '38CB89',
+        border: ' #E8ECEF',
+        'primary-dark': '#0D0E10',
+        'background-dark': '#1C1F24',
+        'secondary-dark': '#2B2E33',
+        'card-dark': '#25282D',
+        'color-dark': '#FFFFFF',
+        'light-dark': '#A8ACB1',
+        'error-dark': '#FF2800',
+        'success-dark': '#00B38F',
+        'border-dark': '#3C4148',
+      },
+    },
+  },
+  plugins: [],
+};
