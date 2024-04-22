6762e3a618d51a1e37ad4c2c9221bd32fd397b99
Madhav Nasit <madhavn@mindfiresolutions.com>
2024-04-22 11:04:46 +0530

    feat: Add formik for form management and yup for validation schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,29 @@ export default {
         'border-dark': '#E8ECEF',
       },
     },
+    // that is animation class
+    animation: {
+      fadeIn: 'fadeIn 0.2s ease-in-out',
+      fadeInDown: 'fadeInDown 0.2s ease-in',
+    },
+
+    // that is actual animation
+    keyframes: (theme) => ({
+      fadeIn: {
+        '0%': { opacity: 0 },
+        '100%': { opacity: 1 },
+      },
+      fadeInDown: {
+        '0%': {
+          opacity: '0',
+          transform: 'translateY(-20px)',
+        },
+        '100%': {
+          opacity: '1',
+          transform: 'translateY(0)',
+        },
+      },
+    }),
   },
   plugins: ['prettier-plugin-tailwindcss'],
 };
