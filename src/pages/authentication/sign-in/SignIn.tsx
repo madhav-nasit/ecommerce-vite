d4871f4ce5130830a0a995dc65a1b022381e9fee
Madhav Nasit <madhavn@mindfiresolutions.com>
2024-04-30 10:57:18 +0530

    fix: Padding for the sign in page form

--- src/pages/authentication/sign-in/SignIn.tsx
+++ src/pages/authentication/sign-in/SignIn.tsx
@@ -73,7 +73,7 @@ export const SignIn: FC = () => {
   });
 
   return (
-    <div className='w-full md:max-w-md'>
+    <div className='w-full p-8 md:max-w-md'>
       {/* Heading section */}
       <h1 className='mb-4 text-3xl font-normal'>{signIn.title}</h1>
       <p className='mb-4 text-sm text-light dark:text-light-dark'>
